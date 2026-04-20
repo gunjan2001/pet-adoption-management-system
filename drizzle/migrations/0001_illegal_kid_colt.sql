86b5894cf3c20ce7c484551877cb0dc92fe8f6f9
gunjan2001 <dalwadigunjan20@gmail.com>
2026-04-20 17:33:25 +0530

    feat: add initial database schema for adoption applications, pets, and users

--- drizzle/migrations/0001_illegal_kid_colt.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE "users" DROP CONSTRAINT "users_openId_unique";--> statement-breakpoint
-ALTER TABLE "adoption_applications" ALTER COLUMN "id" SET DATA TYPE serial;--> statement-breakpoint
-ALTER TABLE "adoption_applications" ALTER COLUMN "id" DROP IDENTITY;--> statement-breakpoint
-ALTER TABLE "pets" ALTER COLUMN "id" SET DATA TYPE serial;--> statement-breakpoint
-ALTER TABLE "pets" ALTER COLUMN "id" DROP IDENTITY;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "id" SET DATA TYPE serial;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "id" DROP IDENTITY;--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN "openId";
